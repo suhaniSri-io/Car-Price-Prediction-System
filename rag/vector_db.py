373d5d23e73e34af9ffcef7a278402be1d13fa7b
Suhani Srivastava <srivastavasuhani2005@gmail.com>
2026-07-02 20:59:48 +0530

    Keep only titanic notebook

--- rag/vector_db.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
